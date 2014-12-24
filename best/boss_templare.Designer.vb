d570dfb766ed0f94eb139f95b2e7eff6001d8015
OdinEidolon <odin.eidolon@hotmail.com>
2014-12-24 01:46:33 +0100

    il random della disperazione

---
+++ best/boss_templare.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class boss_templare
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.cmdAttacca = New System.Windows.Forms.Button()
+        Me.cmdPara = New System.Windows.Forms.Button()
+        Me.lblHp = New System.Windows.Forms.Label()
+        Me.lblTemplare = New System.Windows.Forms.Label()
+        Me.lblPicU = New System.Windows.Forms.Label()
+        Me.lblPicTemp = New System.Windows.Forms.Label()
+        Me.timer_scontro = New System.Windows.Forms.Timer(Me.components)
+        Me.lblDannoTuo = New System.Windows.Forms.Label()
+        Me.lblDannoTemplare = New System.Windows.Forms.Label()
+        Me.timer_animazione = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'cmdAttacca
+        '
+        Me.cmdAttacca.Location = New System.Drawing.Point(51, 220)
+        Me.cmdAttacca.Name = "cmdAttacca"
+        Me.cmdAttacca.Size = New System.Drawing.Size(81, 33)
+        Me.cmdAttacca.TabIndex = 0
+        Me.cmdAttacca.Text = "Attacco"
+        Me.cmdAttacca.UseVisualStyleBackColor = True
+        '
+        'cmdPara
+        '
+        Me.cmdPara.Location = New System.Drawing.Point(138, 220)
+        Me.cmdPara.Name = "cmdPara"
+        Me.cmdPara.Size = New System.Drawing.Size(81, 33)
+        Me.cmdPara.TabIndex = 1
+        Me.cmdPara.Text = "Parata"
+        Me.cmdPara.UseVisualStyleBackColor = True
+        '
+        'lblHp
+        '
+        Me.lblHp.AutoSize = True
+        Me.lblHp.Location = New System.Drawing.Point(12, 21)
+        Me.lblHp.Name = "lblHp"
+        Me.lblHp.Size = New System.Drawing.Size(39, 13)
+        Me.lblHp.TabIndex = 2
+        Me.lblHp.Text = "Label1"
+        '
+        'lblTemplare
+        '
+        Me.lblTemplare.AutoSize = True
+        Me.lblTemplare.Location = New System.Drawing.Point(616, 9)
+        Me.lblTemplare.Name = "lblTemplare"
+        Me.lblTemplare.Size = New System.Drawing.Size(39, 13)
+        Me.lblTemplare.TabIndex = 3
+        Me.lblTemplare.Text = "Label2"
+        '
+        'lblPicU
+        '
+        Me.lblPicU.AutoSize = True
+        Me.lblPicU.Font = New System.Drawing.Font("Courier New", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPicU.Location = New System.Drawing.Point(50, 100)
+        Me.lblPicU.Name = "lblPicU"
+        Me.lblPicU.Size = New System.Drawing.Size(36, 23)
+        Me.lblPicU.TabIndex = 4
+        Me.lblPicU.Text = "tu"
+        Me.lblPicU.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblPicTemp
+        '
+        Me.lblPicTemp.AutoSize = True
+        Me.lblPicTemp.Font = New System.Drawing.Font("Courier New", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPicTemp.Location = New System.Drawing.Point(500, 100)
+        Me.lblPicTemp.Name = "lblPicTemp"
+        Me.lblPicTemp.Size = New System.Drawing.Size(114, 23)
+        Me.lblPicTemp.TabIndex = 5
+        Me.lblPicTemp.Text = "templare"
+        Me.lblPicTemp.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'timer_scontro
+        '
+        Me.timer_scontro.Interval = 10
+        '
+        'lblDannoTuo
+        '
+        Me.lblDannoTuo.AutoSize = True
+        Me.lblDannoTuo.Location = New System.Drawing.Point(89, 84)
+        Me.lblDannoTuo.Name = "lblDannoTuo"
+        Me.lblDannoTuo.Size = New System.Drawing.Size(39, 13)
+        Me.lblDannoTuo.TabIndex = 6
+        Me.lblDannoTuo.Text = "Label2"
+        '
+        'lblDannoTemplare
+        '
+        Me.lblDannoTemplare.AutoSize = True
+        Me.lblDannoTemplare.Location = New System.Drawing.Point(470, 84)
+        Me.lblDannoTemplare.Name = "lblDannoTemplare"
+        Me.lblDannoTemplare.Size = New System.Drawing.Size(39, 13)
+        Me.lblDannoTemplare.TabIndex = 7
+        Me.lblDannoTemplare.Text = "Label2"
+        '
+        'timer_animazione
+        '
+        Me.timer_animazione.Interval = 300
+        '
+        'boss_templare
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(667, 495)
+        Me.Controls.Add(Me.lblDannoTemplare)
+        Me.Controls.Add(Me.lblDannoTuo)
+        Me.Controls.Add(Me.lblPicTemp)
+        Me.Controls.Add(Me.lblPicU)
+        Me.Controls.Add(Me.lblTemplare)
+        Me.Controls.Add(Me.lblHp)
+        Me.Controls.Add(Me.cmdPara)
+        Me.Controls.Add(Me.cmdAttacca)
+        Me.Name = "boss_templare"
+        Me.Text = "boss_templare"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmdAttacca As System.Windows.Forms.Button
+    Friend WithEvents cmdPara As System.Windows.Forms.Button
+    Friend WithEvents lblHp As System.Windows.Forms.Label
+    Friend WithEvents lblTemplare As System.Windows.Forms.Label
+    Friend WithEvents lblPicU As System.Windows.Forms.Label
+    Friend WithEvents lblPicTemp As System.Windows.Forms.Label
+    Friend WithEvents timer_scontro As System.Windows.Forms.Timer
+    Friend WithEvents lblDannoTuo As System.Windows.Forms.Label
+    Friend WithEvents lblDannoTemplare As System.Windows.Forms.Label
+    Friend WithEvents timer_animazione As System.Windows.Forms.Timer
+End Class
---
+++ best/boss_templare.vb
@@ -0,0 +1,118 @@
+﻿Public Class boss_templare
+
+    Dim nome_enemy As String = "Templare"
+    Dim vita_enemy As Integer = 736
+    Dim cap_enemy As Integer = 736
+    Dim attacco_enemy As Integer = 78
+    Dim parata_tua As Boolean = False
+    Dim parata_enemy As Boolean = False
+    Dim posizione As Integer
+    Dim ms As Integer
+    Dim i As Integer = 0
+    Dim start As Integer 'indica quando deve iniziare l'attacco il templare
+
+    Private Sub boss_templare_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+
+        'attacco
+        If e.KeyCode = Keys.A Then
+            lblPicU.Text = "( )" & vbCrLf & " |-!---" & vbCrLf & "/ \"
+            If parata_enemy = False Then
+                vita_enemy -= attacco
+                lblDannoTemplare.Text = attacco
+                lblDannoTemplare.Visible = True
+            Else
+                lblDannoTemplare.Text = 0
+                lblDannoTemplare.Visible = True
+            End If
+        End If
+
+        'parata
+        If e.KeyCode = Keys.D Then
+            lblPicU.Text = "( )" & vbCrLf & " |-|" & vbCrLf & "/ \|"
+            parata_tua = True
+        End If
+
+    End Sub
+
+    Private Sub boss_templare_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+
+        If e.KeyCode = Keys.A Or e.KeyCode = Keys.D Then
+            lblPicU.Text = "( )" & vbCrLf & "-|-" & vbCrLf & "/ \"
+        End If
+
+    End Sub
+
+    Private Sub boss_templare_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.KeyPreview = True 'serve per abilitare la ricezione dei tasti della tastiera (lol)
+
+        posizione = lblPicTemp.Location.X
+        MsgBox(posizione)
+
+        lblDannoTemplare.Visible = False
+        lblDannoTuo.Visible = False
+
+        'per qualche oscuro motivo qui le classi non funziona, perchè non so un cazzo delle classi
+        Randomize()
+
+        start = CInt(Int(Rnd() * 800) + 200)
+
+        lblTemplare.Text = nome_enemy & " " & vita_enemy & "/" & cap_enemy
+        lblHp.Text = "Tu " & ep & "/" & ep_cap
+
+        'lblPicTemp.Text = "( )" & vbCrLf & "---!-| " & vbCrLf & "/ \"
+
+        lblPicU.Text = "( )" & vbCrLf & "-|-" & vbCrLf & "/ \"
+        lblPicTemp.Text = "(†)" & vbCrLf & "-|-" & vbCrLf & "/ \"
+
+        timer_scontro.Enabled = True
+
+    End Sub
+
+    Private Sub timer_scontro_Tick(sender As Object, e As EventArgs) Handles timer_scontro.Tick
+
+        'questo timer serve per gestire l'attacco del templare
+
+        ms += 1
+        lblTemplare.Text = ms
+        lblPicTemp.Text = "(†)" & vbCrLf & "|-| " & vbCrLf & "|/ \"
+
+        If ms > start Then
+            lblDannoTuo.Visible = False
+            parata_enemy = False
+            timer_scontro.Enabled = False
+            timer_animazione.Enabled = True
+
+        End If
+
+    End Sub
+
+    Private Sub timer_animazione_Tick(sender As Object, e As EventArgs) Handles timer_animazione.Tick
+
+        i += 1
+
+        If i = 1 Then
+            lblPicTemp.Text = "(†)" & vbCrLf & "---!-| " & vbCrLf & "/ \"
+        End If
+        'lblPicTemp.Text = lblPicTemp.Text & " "
+        lblPicTemp.Location = New Point(lblPicTemp.Location.X - 50, lblPicTemp.Location.Y)
+        If i = CInt(Int(Rnd() * 6) + 1) Or i = 6 Then 'random che rende tutto più bastardo
+            timer_animazione.Enabled = False
+            If parata_tua = False Then 'visualizzazione della label dei danni e calcolo attacco
+                ep -= attacco_enemy
+                lblDannoTuo.Text = attacco_enemy
+                lblDannoTuo.Visible = True
+            Else
+                lblDannoTuo.Text = 0
+                lblDannoTuo.Visible = True
+            End If
+            ms = 0
+            i = 0
+            start = CInt(Int(Rnd() * 750) + 200)
+            lblPicTemp.Location = New Point(posizione, lblPicTemp.Location.Y)
+            timer_scontro.Enabled = True
+        End If
+
+    End Sub
+
+End Class
